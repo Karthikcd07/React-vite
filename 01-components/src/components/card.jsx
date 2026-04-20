db49d2383d0ce51945a1d30c40a10d2e1be087fe
Karthik <karthikcd307@gmail.com>
2026-04-20 22:18:04 +0530

    Explored components in React

---
+++ 01-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      <div class='card'>
+        Ich icze Karthik,Ich Komme aus Indien,Ich wohne in Banglore,
+        Ich spreache Englicsh,Kannada und ein bischeen Deutsch
+      </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 01-components/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <div class='nav'>
+        Ich bin ein und zwanzig yahre alt.
+        Meine hobby ist spielen Cricket.
+      </div>
+    </div>
+  )
+}
+
+export default navbar
